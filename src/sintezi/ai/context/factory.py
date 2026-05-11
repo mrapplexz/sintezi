c5bf28ac8d9bed01272eac1eb814ff1e1ee99db3
mrapplexz <mr.applexz@gmail.com>
2026-05-12 02:38:52 +0300

    add custom retry policy

--- src/sintezi/ai/context/factory.py
+++ src/sintezi/ai/context/factory.py
@@ -3,10 +3,12 @@ import openai
 from sintezi.ai.parser import ResponseFormatCapability
 
 from .ctx import AiContext
+from .retry import RetryPolicy
 
 
-def ai_context_from_openai(client: openai.AsyncClient) -> AiContext:
+def ai_context_from_openai(client: openai.AsyncClient, retry_policy: RetryPolicy | None = None) -> AiContext:
     return AiContext(
         client=client,
         response_format_capabilities={ResponseFormatCapability.json, ResponseFormatCapability.json_schema},
+        retry_policy=retry_policy,
     )
